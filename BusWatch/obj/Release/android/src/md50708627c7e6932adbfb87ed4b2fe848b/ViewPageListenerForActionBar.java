e8928470e7da67b1c7d253e909b039ce8079c673
Michael Zhang <michaelxmail@gmail.com>
2017-01-15 14:41:34 -0700

    recompiled: select release, rebuld the projrct, then use menu to export the apk file

---
+++ BusWatch/obj/Release/android/src/md50708627c7e6932adbfb87ed4b2fe848b/ViewPageListenerForActionBar.java
@@ -0,0 +1,54 @@
+package md50708627c7e6932adbfb87ed4b2fe848b;
+
+
+public class ViewPageListenerForActionBar
+	extends android.support.v4.view.ViewPager.SimpleOnPageChangeListener
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onPageSelected:(I)V:GetOnPageSelected_IHandler\n" +
+			"";
+		mono.android.Runtime.register ("BusWatch.ViewPageListenerForActionBar, BusWatch, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", ViewPageListenerForActionBar.class, __md_methods);
+	}
+
+
+	public ViewPageListenerForActionBar () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == ViewPageListenerForActionBar.class)
+			mono.android.TypeManager.Activate ("BusWatch.ViewPageListenerForActionBar, BusWatch, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+	public ViewPageListenerForActionBar (android.app.ActionBar p0) throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == ViewPageListenerForActionBar.class)
+			mono.android.TypeManager.Activate ("BusWatch.ViewPageListenerForActionBar, BusWatch, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "Android.App.ActionBar, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065", this, new java.lang.Object[] { p0 });
+	}
+
+
+	public void onPageSelected (int p0)
+	{
+		n_onPageSelected (p0);
+	}
+
+	private native void n_onPageSelected (int p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
